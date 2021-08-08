e87b798583fe7362e051c58b781b97a3c5303c26
Alejandro Rodriguez <alejandro.rdrgz.e@gmail.com>
2021-08-08 13:34:42 -0500

    Add new sections

--- src/app/modules/survey/components/survey/survey.component.ts
+++ src/app/modules/survey/components/survey/survey.component.ts
@@ -9,7 +9,66 @@ export class SurveyComponent {
   rfc: string;
   acta: string;
   representante: string;
+
   actaRepresentante: string;
   sector: string;
+  tipoInmueble: string;
+  tipoConstruccion: string;
+  niveles: string;
+  poblacion: string;
+  visitantes: string;
+  discapacidad: string;
+  ninos: string;
+  constructivo: string;
+  modificaciones: string;
+  especiales: string;
+  elevadores: string;
+  recipientes: string;
+ 
+  puntoReunion: string;
+  analisisRiesgo: string;
+  simulacros: string;
+  estudioRiesgos: string;
+  contingencia: string;
+  planContinudad: string;
+  corresponsable: string;
+  corresponsabilidad: string;
+  polizaSeguro: string;
+  vobo: string;
+  noModificacion: string;
+  gas: string;
+  nomDos: string;
+  botiquin: string;
+  alertamiento: string;
+  cartaAlertamiento: string;
+  estructural: string;
+  
+
+  brigadaemerg: string;
+  capacitacion: string;
+  simulacro: string;
+  mantenimiento: string;
+  arquitectonicos: string;
+  vulnerables: string;
+  planoBotiquines: string;
+  planoBrigadistas: string;
+  riskElectricos: string;
+  planoEvacuacion: string;
+  planoEscaleras: string;
+  planoAlertamiento: string;
+  planoRisk: string;
+  planoNomCeroDos: string;
+  extintores: string;
+  planoMenorrisk: string;
+
+
+  confiaBrigada: string;
+  confiaAcreditado: string;
+  continuidadMercado: string;
+  recursosEmergencia: string;
+  preparados: string;
+
+
+
   seccion1= false;
 }
